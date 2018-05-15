481ef0c855e24c196ba0748fe042d60f8fbfdf3b
Nina <ninkaperegon@gmail.com>
2018-05-15 15:48:51 +0300

    Registration and login. Login view

---
+++ AestheticGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AestheticGallery.Startup))]
+namespace AestheticGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
